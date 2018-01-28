aeaa81f9dbb636d92ec1525caea877ff48ab350c
David Goldfarb <deg@degel.com>
2018-01-28 11:54:28 +0200

    Working picker

--- src/receipts_native/core.cljs
+++ src/receipts_native/core.cljs
@@ -6,12 +6,47 @@
 
 (def ReactNative (js/require "react-native"))
 
+(defn get-react-property [name]
+  (aget ReactNative name))
+
+(defn adapt-class [class]
+  (when class
+    (r/adapt-react-class class)))
+
+(defn get-class [name]
+  (adapt-class (get-react-property name)))
+
 (def app-registry (.-AppRegistry ReactNative))
-(def text (r/adapt-react-class (.-Text ReactNative)))
-(def view (r/adapt-react-class (.-View ReactNative)))
-(def image (r/adapt-react-class (.-Image ReactNative)))
-(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
+(def text (get-class "Text"))
+(def view (get-class "View"))
+(def image (get-class "Image"))
+(def touchable-highlight (get-class "TouchableHighlight"))
 (def Alert (.-Alert ReactNative))
+
+
+
+(def picker (get-class "Picker"))
+(def picker-item (adapt-class (.-Item (get-react-property "Picker"))))
+
+
+(def text-input-class (get-class "TextInput"))
+
+(defn text-input [{:keys [font style] :as opts
+                   :or   {font :default}} text]
+  [text-input-class (merge
+                     {:underline-color-android :transparent
+                      :placeholder-text-color  "red"
+                      :placeholder             "type a message"
+                      :value                   text}
+                     (-> opts
+                         (dissoc :font)
+                         (assoc :style (merge style font))))])
+
+#_(def flat-list-class (get-class "FlatList"))
+#_(def section-list-class (get-class "SectionList"))
+
+
+
 
 (defn alert [title]
   (.alert Alert title))
@@ -21,9 +56,20 @@
     (fn []
       [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
        [image {:source (js/require "./assets/images/cljs.png")
-               :style {:width 200
-                       :height 200}}]
-       [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
+               :style {:width 50
+                       :height 50}}]
+       [picker {:style {:width 350 :height 50 :margin 10}
+                :selected-value :b
+                :on-value-change #(js/alert (str "Got: " %1 ", pos=" %2))}
+        [picker-item {:value :a :label "Alabama"}]
+        [picker-item {:value :b :label "Baltimore"}]
+        [picker-item {:value :c :label "Canada"}]]
+       #_[flat-list-class {:data [{:key :a}
+                                {:key :b}
+                                {:key :c}]
+                         :render-item (fn [x] (:key x))}]
+       [text-input-class]
+       [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting "yyy"]
        [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
                              :on-press #(alert "HELLO!")}
         [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
